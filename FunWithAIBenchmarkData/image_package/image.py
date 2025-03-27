0429b65bd9cfc106c3e3aca2c385083ce23c69ff
cmramsey3 <coltonramsey3@gmail.com>
2025-03-26 20:39:57 -0400

    Added Documentation to image.py

--- FunWithAIBenchmarkData/image_package/image.py
+++ FunWithAIBenchmarkData/image_package/image.py
@@ -1,3 +1,18 @@
+# File Name: image.py
+# Student Name: Colton Ramsey, Lucas Iceman, Zach Bell
+# email: ramseyc6@mail.uc.edu, icemanlc@mail.uc.edu, bellzj@mail.uc.edu
+# Assignment Number: Assignment 08
+# Due Date: 03/25/2025
+# Course #/Section: IS 4010-001
+# Semester/Year: Spring 2025
+# Brief Description of the assignment: Modify a VS Python project to add some data visualization.
+
+# Brief Description of what this module does: This module creates a function to be called in the main that opens up 
+# the image of Blastoise the pokemon.
+# Citations: ChatGPT to understand how to show an image.
+# Anything else that's relevant: None
+
+
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
  
